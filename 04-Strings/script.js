92b57b96263237ed47ee23aaabd1991176af7c1b
waseemriyazz <waseemriyaz717@gmail.com>
2023-07-30 12:17:16 +0530

    Basic strings

---
+++ 04-Strings/script.js
@@ -0,0 +1,7 @@
+let Name = "Waseem";
+console.log(Name);
+let backtick = `this is a backtick string`
+console.log(backtick);
+let number = 12;
+let NumberInString = `this is a number concatinated within a string ${number}`;
+console.log(NumberInString);
